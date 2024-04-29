da74e0ab5b96570ea32e7882fef1589c3f89c68c
bawanetejas <tejasbawane221@gmail.com>
2024-04-30 01:24:46 +0530

    add BE url to FE

--- src/Socket/SocketContext.jsx
+++ src/Socket/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContexProvider = ({children})=>{
 
     useEffect(()=>{
         if(user){
-            const socket = io("http://localhost:4000",{
+            const socket = io("https://chatapp-pt7r.onrender.com",{
                 
                 query:{
                     userId:user._id,
